3da382a6c05df3910aa135b05a722e87337faaeb
Ludo <ludovico.ferrara@ruhr-uni-bochum.de>
2022-04-28 18:43:25 +0200

    udpated empty.java & empty.class to not be empty :)

--- empty.java
+++ empty.java
@@ -1,6 +1,7 @@
 public class empty {
     public static void main(String[] args) {
-        int i = 5/2;
+        int i = 5 / 2;
+        System.out.println("Line print test");
         System.out.print("Die Variable i hat den Wert ");
         System.out.print(i);
     }
