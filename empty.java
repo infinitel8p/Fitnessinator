c3707859f8383442023114358726eb78389feeef
Infinitel8p <bigludovico@aol.com>
2022-04-28 14:53:19 +0200

    added empty.java as placeholder

---
+++ empty.java
@@ -0,0 +1,7 @@
+public class empty {
+    public static void main(String[] args) {
+        int i = 5/2;
+        System.out.print("Die Variable i hat den Wert ");
+        System.out.print(i);
+    }
+}
