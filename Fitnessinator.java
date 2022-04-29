a697b109e114a7e844cda7180febdf38490bfdec
Infinitel8p <bigludovico@aol.com>
2022-04-29 18:00:57 +0200

    updated code

--- Fitnessinator.java
+++ Fitnessinator.java
@@ -10,29 +10,37 @@ public class Fitnessinator {
         // println das Men
         System.out.println(
                 "Men\u00fc:\n1 = Mein Trainingsplan\n2 = Neuer Eintrag\n3 = Eintrag l\u00f6schen\n4 = Beenden\n\nBitte w\u00e4hle eine Zahl");
-        // erstelle einen scanner und lasse input fordern
-        Scanner scannerVariable = new Scanner(System.in);
-        //erstelle strings mit auszugebenden Text
-        String result = "Sie haben gew\u00e4hlt: ";
-        String scanned_text = scannerVariable.nextLine();
-        // if abfragen
-        /*
-        scanned_text wird mit .equals(x) auf gleichheit mit x verglichen weil ->
-        "Strings and Boxed types should be compared using "equals()""
-        String vergleiche mit x==y sind enden false, selbst wenn x und y die gleiche value haben
-        - unten testbar mit 4
+        /* 
+        im folgenden wollen wir eine eingabe vom user bekommen
+        dafür nutzen wir den importierten scanner
+        wenn man einen scanner verwendet soll man ihn wieder schließen (nicht schließen gibt folgende fm in manchen lintern: Resource leak: 'scanner Variable' is never closed)
+        man muss ihn nicht schließen, das programm funktioniert auch so aber anscheinend best practice es einfach zu machen
+        wenn man das scannen und verarbeiten in einen try block schreibt wird der scanner anschließend automatisch geschlossen, deswegen hier das try
         */
-        if (scanned_text.equals("1")) {
-            //wenn eingabe 1 war dann wird MeinTrainingsplan.java aufgerufen
-            MeinTrainingsplan.main(args);
-        } else if (scanned_text.equals("2")) {
-            System.out.println(result + scanned_text);
-        } else if (scanned_text.equals("3")) {
-            System.out.println(result + scanned_text);
-        } else if (scanned_text == "4") {
-            System.out.println(result + scanned_text);
-        } else {
-            System.out.println("Eingabe nicht erkannt! -> " + scanned_text);
+        try (
+            // erstelle einen scanner und lasse input fordern
+            Scanner scannerVariable = new Scanner(System.in)) {
+            //erstelle strings mit auszugebenden Text
+            String result = "Sie haben gew\u00e4hlt: ";
+            String scannedText = scannerVariable.nextLine();
+            /*
+            scannedText wird mit .equals(x) auf gleichheit mit x verglichen ("Strings and Boxed types should be compared using "equals()"")
+            String vergleiche mit x==y sind enden false, selbst wenn x und y die gleiche value haben,
+            deswegen x.equals(y)
+            - unten testbar wenn man bei der abfrage 4 eintippt (1-3 tun was sie sollen, 4 sollte auch den text ausgeben, stattdessen kommt die else ausgabe)
+            */
+            if (scannedText.equals("1")) {
+                //wenn eingabe 1 war dann wird MeinTrainingsplan.java aufgerufen
+                MeinTrainingsplan.main(args);
+            } else if (scannedText.equals("2")) {
+                System.out.println(result + scannedText);
+            } else if (scannedText.equals("3")) {
+                System.out.println(result + scannedText);
+            } else if (scannedText == "4") {
+                System.out.println(result + scannedText);
+            } else {
+                System.out.println("Eingabe nicht erkannt! -> " + scannedText);
+            }
         }
     }
 }
